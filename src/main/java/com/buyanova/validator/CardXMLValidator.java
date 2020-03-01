040d8d96fc468a5881da4fb99a40982da9e10fd6
Natalie <natalli.buyanova.01@mail.ru>
2020-03-01 23:40:51 +0300

    Add xml validator based on xsd schema

---
+++ src/main/java/com/buyanova/validator/CardXMLValidator.java
@@ -0,0 +1,30 @@
+package com.buyanova.validator;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
+public class CardXMLValidator {
+
+    public boolean validate(String fileName, String schemaName) {
+        String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+        SchemaFactory factory = SchemaFactory.newInstance(language);
+        File schemaLocation = new File(schemaName);
+        try {
+            Schema schema = factory.newSchema(schemaLocation);
+            Validator validator = schema.newValidator();
+            Source source = new StreamSource(fileName);
+            validator.validate(source);
+            return true;
+        } catch (SAXException | IOException e) {
+            return false;
+        }
+    }
+}
